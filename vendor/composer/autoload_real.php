fa829d36b148d7e05d6de593492d900fd8ee3e47
Harlock <iscprimerocesar@gmail.com>
2024-09-09 20:03:01 +0000

    update server

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -33,18 +33,25 @@ class ComposerAutoloaderInitace3a0b8bf2565cc80d502c5ce5f057d
 
         $loader->register(true);
 
-        $filesToLoad = \Composer\Autoload\ComposerStaticInitace3a0b8bf2565cc80d502c5ce5f057d::$files;
-        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
-            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
-
-                require $file;
-            }
-        }, null, null);
-        foreach ($filesToLoad as $fileIdentifier => $file) {
-            $requireFile($fileIdentifier, $file);
+        $includeFiles = \Composer\Autoload\ComposerStaticInitace3a0b8bf2565cc80d502c5ce5f057d::$files;
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequireace3a0b8bf2565cc80d502c5ce5f057d($fileIdentifier, $file);
         }
 
         return $loader;
+    }
+}
+
+/**
+ * @param string $fileIdentifier
+ * @param string $file
+ * @return void
+ */
+function composerRequireace3a0b8bf2565cc80d502c5ce5f057d($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+        require $file;
     }
 }
